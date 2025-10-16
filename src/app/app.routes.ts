6ecd3f7eafd2f19e991e348a9880afa8c162bb4e
Carlotagf <carlotagf00@gmail.com>
2025-10-16 18:04:38 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Carrera } from './carrera/carrera';
 import { EyeCandy } from './eye-candy/eye-candy';
 import { Lista } from './ListaPersonajes/lista/lista';
 import { FormularioRegistro } from './formulario-registro/formulario-registro';
+import { FormularioHalloween } from './formulario-halloween/formulario-halloween';
 
 export const routes: Routes = [
     {path: 'contador', component: Contador},
@@ -12,7 +13,8 @@ export const routes: Routes = [
     {path: 'carrera', component: Carrera},
     {path: 'eyecandy', component: EyeCandy},
     {path: 'ListaPersonajes', component: Lista},
-    {path: 'formulario-registro', component: FormularioRegistro}
+    {path: 'formulario-registro', component: FormularioRegistro},
+    {path: 'formulario-halloween', component: FormularioHalloween}
     
 
 ];
---
+++ src/app/formulario-halloween/formulario-halloween.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-halloween',
+  imports: [],
+  templateUrl: './formulario-halloween.html',
+  styleUrl: './formulario-halloween.css'
+})
+export class FormularioHalloween {
+
+}
