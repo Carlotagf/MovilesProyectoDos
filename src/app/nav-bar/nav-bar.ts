79a4afca84a6e729c0eb48178bdf41750bd1a37c
Carlotagf <carlotagf00@gmail.com>
2025-10-17 17:03:26 +0200

    up

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterLink } from "@angular/router";
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatToolbarModule, RouterLink, MatIconModule ],
+  imports: [FormsModule, MatSlideToggleModule, CommonModule, MatToolbarModule, RouterLink, MatIconModule, MatMenuModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
+  esHalloween: boolean = false;
 
 }
