60de6e6adac294d3a81c2a5ad06d3d42dcee54b7
Carlotagf <carlotagf00@gmail.com>
2025-10-23 16:31:36 +0200

    up

--- src/app/kanban/kanban-board/kanban-board.ts
+++ src/app/kanban/kanban-board/kanban-board.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { DragDropModule, CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [],
+  imports: [CdkDrag, DragDropModule],
   templateUrl: './kanban-board.html',
   styleUrl: './kanban-board.css'
 })
 export class KanbanBoard {
+
+  movies = [
+    'Levantarse'
+  ]
 
 }
