345f003f7192b6bfaf569436911fe79aaee629db
Carlotagf <carlotagf00@gmail.com>
2025-12-10 13:11:13 +0100

    up

--- src/app/matatopos/matatopos/matatopos.ts
+++ src/app/matatopos/matatopos/matatopos.ts
@@ -10,11 +10,32 @@ import { Component } from '@angular/core';
 export class Matatopos {
 
   topos : number[] = [1,0,0,0,0,0,0,0,0];
+  contador:number = 0;
 
    
+  
+
   mataTopo(index:number){
-    console.log(index);
+    let nuevoTopo = this.devolverNuevoTopo(index);
+    this.topos=[0,0,0,0,0,0,0,0,0];
+    this.topos[nuevoTopo]=1;
+    this.contador++;
   }
+
+  devolverNuevoTopo(i:number){
+    let nuevoTopo=Math.floor(Math.random()*9);
+    
+    while (nuevoTopo==i){
+      nuevoTopo=Math.floor(Math.random()*9);
+    }
+    return nuevoTopo;
+
+  }
+
+  fallo(){
+    this.contador--;
+  }
+
 
   
 
