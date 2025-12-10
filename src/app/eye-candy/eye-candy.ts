d8a9a156e40dca2ba4a7bff68a3cd5a38e118a13
Carlotagf <carlotagf00@gmail.com>
2025-12-10 12:49:21 +0100

    up

--- src/app/eye-candy/eye-candy.ts
+++ src/app/eye-candy/eye-candy.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-eye-candy',
   imports: [],
@@ -7,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrl: './eye-candy.css'
 })
 export class EyeCandy {
+ 
+  
+  setupCarousel(): void {
+    
+    const nextButton = document.querySelector<HTMLButtonElement>(".next");
+    const prevButton = document.querySelector<HTMLButtonElement>(".prev");
+
+    
+    const slideContainer = document.querySelector<HTMLElement>(".slide");
+
+    if (!nextButton || !prevButton || !slideContainer) {
+        console.error("No se encontraron elementos esenciales para el carrusel (.next, .prev, o .slide).");
+        return;
+    }
+
+    
+    nextButton.addEventListener("click", () => {
+        
+        const items: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(".item");
+        
+        
+        if (items.length > 0) {
+            slideContainer.appendChild(items[0]);
+        }
+    });
+
+
+    prevButton.addEventListener("click", () => {
+        
+        const items: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(".item");
+        
+        
+        if (items.length > 0) {
+            const lastItem = items[items.length - 1];
+            slideContainer.prepend(lastItem);
+        }
+    });
+}
+
+
+
 
 }
